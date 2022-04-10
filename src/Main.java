17e89591aba773c920f3bf790bd252400b6d8a5d
Zhandos Yessengaziev <zesengaziev@outlook.com>
2022-04-10 18:42:19 +0600

    Announcement of Plagiarismnetics 1.1: Uploading all source files of project

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,5 @@ public class Main {
     public static void main(String[] args) {
         GeneralFrame generalFrame = new GeneralFrame();
         generalFrame.setVisible(true);
-        System.out.println("Hello, World");
     }
 }
